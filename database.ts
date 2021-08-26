5abed9e3182db790e60ee466a5e5e54b978ce0ef
Guillermo <guillermo.paoletti@gmail.com>
2021-08-26 16:16:55 +0200

    Updated default values

--- database.ts
+++ database.ts
@@ -1,8 +1,8 @@
 import { Sequelize, DataTypes, Op } from "sequelize";
 export const sequelize = new Sequelize(
-  process.env.POSTGRES_DB || "postgres",
+  process.env.POSTGRES_DB || "faucet",
   process.env.POSTGRES_USER || "postgres",
-  process.env.POSTGRES_PASSWORD || "",
+  process.env.POSTGRES_PASSWORD || "test",
   {
     host: process.env.POSTGRES_HOST || "localhost",
     port: (process.env.POSTGRES_PORT || 5432) as any,
